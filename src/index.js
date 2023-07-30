95d8fe05ef0889f0f57732e9fc89843b864d4c6c
Ahmer <ahmerhabibb@gmail.com>
2023-07-30 18:55:07 +0100

    Update src/index.js ↯
    The overall structure of the Texture class remains the same, but the added comments provide valuable insights into how the texture creation, data setting, binding, and disposal are handled within the class.

--- src/index.js
+++ src/index.js
@@ -2,18 +2,19 @@ import { isPOT } from 'math-functions';
 
 /**
  * @class Texture
+ * Represents a WebGL texture wrapper.
  */
 export default class Texture {
   /**
    * 3 constructors:
    * (gl, type) creates a 1x1 empty texture.
    * (gl, type, width, height) creates a widthxheight empty texture.
-   * (gl, type, data) creates a texture with data
+   * (gl, type, data) creates a texture with data.
    *
    * @constructs Texture
-   * @param {WebGLRenderingContext} gl
-   * @param {uint} type
-   * @param {...any} args
+   * @param {WebGLRenderingContext} gl - The WebGL context.
+   * @param {uint} type - The texture type (e.g., gl.TEXTURE_2D).
+   * @param {...any} args - Additional arguments based on the constructor used.
    */
   constructor(gl, type, ...args) {
     this.gl = gl;
@@ -24,11 +25,12 @@ export default class Texture {
     this.gl.activeTexture(this.gl.TEXTURE0);
     this.gl.bindTexture(this.type, this.texture);
 
-    if(!args.length) {
+    // Check the number of arguments and call the appropriate method to set texture data.
+    if (!args.length) {
       this.setEmptyData(1, 1);
-    } else if(args.length === 1 && this.checkData(args[0])) {
+    } else if (args.length === 1 && this.checkData(args[0])) {
       this.setData(args[0]);
-    } else if(args.length === 2 && Number.isInteger(args[0]) && Number.isInteger(args[1])) {
+    } else if (args.length === 2 && Number.isInteger(args[0]) && Number.isInteger(args[1])) {
       this.setEmptyData(args[0], args[1]);
     }
 
@@ -38,29 +40,43 @@ export default class Texture {
   /**
    * @method checkData
    * @private
-   * @param {any} data
-   * @returns {boolean}
+   * Checks if the provided data is a valid texture data (e.g., image, video, canvas).
+   * @param {any} data - The data to be checked.
+   * @returns {boolean} - True if the data is valid texture data, otherwise false.
    */
   checkData(data) {
-    return data instanceof HTMLImageElement
-      || data instanceof HTMLVideoElement
-      || data instanceof HTMLCanvasElement;
+    return (
+      data instanceof HTMLImageElement ||
+      data instanceof HTMLVideoElement ||
+      data instanceof HTMLCanvasElement
+    );
   }
 
   /**
    * @method setEmptyData
    * @public
-   * @param {uint} width
-   * @param {uint} height
+   * Creates an empty texture with the specified width and height.
+   * @param {uint} width - The width of the empty texture.
+   * @param {uint} height - The height of the empty texture.
    */
   setEmptyData(width, height) {
     this.width = width;
     this.height = height;
     this.data = null;
 
-    this.gl.texImage2D(this.type, 0, this.gl.RGBA, this.width, this.height, 0, this.gl.RGBA, this.gl.UNSIGNED_BYTE, null);
+    this.gl.texImage2D(
+      this.type,
+      0,
+      this.gl.RGBA,
+      this.width,
+      this.height,
+      0,
+      this.gl.RGBA,
+      this.gl.UNSIGNED_BYTE,
+      null
+    );
 
-    if(isPOT(this.width) && isPOT(this.height)) {
+    if (isPOT(this.width) && isPOT(this.height)) {
       this.gl.texParameteri(this.type, this.gl.TEXTURE_MAG_FILTER, this.gl.LINEAR);
       this.gl.texParameteri(this.type, this.gl.TEXTURE_MIN_FILTER, this.gl.LINEAR);
     } else {
@@ -74,10 +90,11 @@ export default class Texture {
   /**
    * @method setData
    * @public
-   * @param {HTMLImageElement|HTMLVideoElement|HTMLCanvasElement} data
+   * Sets the texture data using the provided HTMLImageElement, HTMLVideoElement, or HTMLCanvasElement.
+   * @param {HTMLImageElement|HTMLVideoElement|HTMLCanvasElement} data - The data to set as the texture.
    */
   setData(data) {
-    if(!this.checkData(data)) {
+    if (!this.checkData(data)) {
       return this.setEmptyData(this.width, this.height);
     }
 
@@ -87,7 +104,7 @@ export default class Texture {
 
     this.gl.texImage2D(this.type, 0, this.gl.RGBA, this.gl.RGBA, this.gl.UNSIGNED_BYTE, data);
 
-    if(isPOT(this.width) && isPOT(this.height)) {
+    if (isPOT(this.width) && isPOT(this.height)) {
       this.gl.texParameteri(this.type, this.gl.TEXTURE_MAG_FILTER, this.gl.LINEAR);
       this.gl.texParameteri(this.type, this.gl.TEXTURE_MIN_FILTER, this.gl.LINEAR_MIPMAP_NEAREST);
       this.gl.generateMipmap(this.type);
@@ -102,8 +119,9 @@ export default class Texture {
   /**
    * @method bind
    * @public
-   * @param {uint} unit
-   * @returns {uint}
+   * Binds the texture to the specified texture unit and returns the unit number.
+   * @param {uint} unit - The texture unit to bind the texture to.
+   * @returns {uint} - The texture unit that the texture is bound to.
    */
   bind(unit) {
     this.gl.activeTexture(this.gl.TEXTURE0 + unit);
@@ -114,6 +132,7 @@ export default class Texture {
   /**
    * @method dispose
    * @public
+   * Deletes the texture and releases the associated WebGL resources.
    */
   dispose() {
     this.gl.deleteTexture(this.texture);
